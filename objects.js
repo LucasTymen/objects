057e0dcddb990de7ee6ef97144eaf207765a4e07
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-06 14:04:14 +0100

    Accessing_Properties_with_dot

--- objects.js
+++ objects.js
@@ -1,1 +1,17 @@
 // Introduction to Objects
+
+// Basics - construction of an object
+let fasterShip = {color: 'silver', 'Fuel Type': 'Turbo Fuel'};
+
+// Accessing Properties wit dot access ()
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew = {};
+let planetArray = spaceship.flightPath = {}

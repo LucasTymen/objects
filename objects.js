04ee4e1dd07cc57285fbe4c2fcb41c736370e20f
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-06 20:04:07 +0100

    objects - braket notation

--- objects.js
+++ objects.js
@@ -15,3 +15,21 @@ let spaceship = {
 // Write your code below
 let crewCount = spaceship.numCrew = {};
 let planetArray = spaceship.flightPath = {}
+
+// #################### Bracket Notation ####################
+let spaceshipé = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+// Write your code below
+let isActive = spaceshipé["Active Mission"];
+
+let returnAnyProp = (objectName, propName) => objectName[propName];
+console.log(returnAnyProp(spaceshipé, "homePlanet"));
+
+console.log(isActive)

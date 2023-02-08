acdef2fd46f14ec818b285eb4bcecff5f0ecae8a
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-08 11:36:37 +0100

    LoopingThroughObjects_For_IN

--- objects.js
+++ objects.js
@@ -183,5 +183,60 @@ greenEnergy(spaceship)
 remotelyDisable(spaceship)
 
 console.log(spaceship)
+*/
+
+// #################### Looping Through Objects (FOR...IN) ####################
+/*
+// syntax & exemple of iteration :
+for (let variableName in outerObject.innerObject) {
+  console.log(`${outerObject.innerObject[variableName].propertyName}: ${outerObject.innerObject[variableName].differentPropertyName}`)
+};
+
+
+let spaceship = {
+  crew: {
+    captain: {
+      name: "Lily",
+      degree: "Computer Engineering",
+      cheerTeam() {
+        console.log("You got this!");
+      },
+    },
+    "chief officer": {
+      name: "Dan",
+      degree: "Aerospace Engineering",
+      agree() {
+        console.log("I agree, captain!");
+      },
+    },
+    medic: {
+      name: "Clementine",
+      degree: "Physics",
+      announce() {
+        console.log(`Jets on!`);
+      },
+    },
+    translator: {
+      name: "Shauna",
+      degree: "Conservation Science",
+      powerFuel() {
+        console.log("The tank is full!");
+      },
+    },
+  },
+};
+
+// Write your code below
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+console.log("#################################");
+for (let crewMember in spaceship.crew) {
+  console.log(
+    `${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`
+  );
+}
+
+
 
 */
